bb1c0e5a4d1fced998c0c7171e6c261ce51eea4a
MubarekHussen <mubahussen2014@gmail.com>
2023-12-01 19:05:47 +0300

    feat: adding more visualaizations

--- streamlit_data_display/streamlit_app.py
+++ streamlit_data_display/streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
+import streamlit.components.v1 as components
 
 
 def fetch_data(url):
@@ -15,7 +16,7 @@ def fetch_data(url):
 
 
 def main():
-    st.title('Visualizations from FastAPI')
+    st.title('Slack Data Analysis')
 
     # Fetching sentiment data from FastAPI
     sentiment_url = "http://127.0.0.1:8000/sentiment"
@@ -32,9 +33,9 @@ def main():
     # Fetching reaction count from FastAPI
     reaction_count_url = "http://127.0.0.1:8000/reaction_count"
     reaction_count = fetch_data(reaction_count_url)
-    if sentiment is not None and message_count is not None and topics is not None and reaction_count is not None:
+    if all(data is not None for data in [sentiment, message_count, topics, reaction_count]):
         # Creating a Word Cloud
-        st.subheader('Word Cloud')
+        st.subheader('Message Count Analysis')
         wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(
             dict(zip(message_count['sender_name'], message_count['count']))
         )
@@ -46,17 +47,25 @@ def main():
         st.pyplot(fig)
 
         # Creating a Seaborn pairplot for data exploration
-        st.subheader('Pair Plot')
+        st.subheader('Sentiment Analysis')
         pair_plot = sns.pairplot(sentiment)
         st.pyplot(pair_plot)
 
         # Creating a pie chart for reaction_count
-        st.subheader('Pie Chart')
+        st.subheader('Reaction Count Analysis')
         fig, ax = plt.subplots()
         ax.pie(reaction_count['reaction_count'], labels=reaction_count['user'], autopct='%1.1f%%')
         st.pyplot(fig)
     else:
         st.error("Failed to fetch data from FastAPI.")
+
+    # Load the HTML from the file
+    with open('lda_visualization.html', 'r') as f:
+        html = f.read()
+
+    # Display the HTML in Streamlit
+    st.subheader('Topics Analysis')
+    components.html(html, width=900, height=800, scrolling=True)
 
 
 if __name__ == '__main__':
